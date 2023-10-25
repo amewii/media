df051a0f90a5e0d1fb28bd8ebf26d4ddf4e873c5
Amri <amriamewii@gmail.com>
2023-10-25 19:20:38 +0800

    amrimac25oct20235

--- admin/config.js
+++ admin/config.js
@@ -7,10 +7,10 @@
 
 if(window.location.hostname == "10.1.3.36"){
     var host = window.location.protocol+"//"+window.location.hostname+"/api_asdcm/public/";
-} else if(window.location.hostname == "admin.media.intan.my"){
+} else if(window.location.hostname == "media.intan.my"){
     var host = window.location.protocol+"//"+window.location.hostname+"/api_asdcm/public/";
 }
-else if(window.location.port != "80"){
+else if(window.location.port == "8081"){
     var host = window.location.protocol+"//"+window.location.hostname+":8081/media/api_asdcm/public/";
 } else {
     var host = window.location.protocol+"//"+window.location.hostname+"/media/api_asdcm/public/";
--- user/config.js
+++ user/config.js
@@ -2,7 +2,17 @@
 // var host = "https://media.intan.my/api_asdcm/";
 
 // SERVER
-var host = "http://localhost/media/api_asdcm/";
+// var host = "http://localhost/media/api_asdcm/";
+if(window.location.hostname == "10.1.3.36"){
+    var host = window.location.protocol+"//"+window.location.hostname+"/api_asdcm/public/";
+} else if(window.location.hostname == "admin.media.intan.my"){
+    var host = window.location.protocol+"//"+window.location.hostname+"/api_asdcm/public/";
+}
+else if(window.location.port == "8081"){
+    var host = window.location.protocol+"//"+window.location.hostname+":8081/media/api_asdcm/public/";
+} else {
+    var host = window.location.protocol+"//"+window.location.hostname+"/media/api_asdcm/public/";
+}
 
 function getUrlVars() {
     var vars = {};
