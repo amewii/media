710a1b94ede34a92f964eb24da801beb088a18ef
AfrinaRizal <afrinarizal00@gmail.com>
2025-01-19 18:35:45 +0800

    afrina19012024-1829pm

--- user/api_asdcm/app/Http/Controllers/med_programController.php
+++ user/api_asdcm/app/Http/Controllers/med_programController.php
@@ -303,9 +303,9 @@ class med_programController extends Controller
                 if(sizeof($file)>0){
                     for($j=0;$j<sizeof($file);$j++){
                         $url = $host.$file[$j]->images;
+                        dd($url);
                         $handle = curl_init($url);
                         curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
-                        dd($handle);
 
                         /* Get the HTML or whatever is linked in $url. */
                         $response = curl_exec($handle);
