d22727f450f1b7b0558ad8cc2e70a6ccc1e47605
AfrinaRizal <afrinarizal00@gmail.com>
2025-01-19 17:02:16 +0800

    afrina19012025-1609pm

--- user/api_asdcm/app/Http/Controllers/med_programController.php
+++ user/api_asdcm/app/Http/Controllers/med_programController.php
@@ -305,23 +305,29 @@ class med_programController extends Controller
                 if(sizeof($file)>0){
                     for($j=0;$j<sizeof($file);$j++){
                         $url = $host.$file[$j]->images;
+                        dd($url);
+
                         $handle = curl_init($url);
+
                         curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
                         
                         /* Get the HTML or whatever is linked in $url. */
                         $response = curl_exec($handle);
+
                         
                         /* Check for 404 (file not found). */
                         $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
                         if($httpCode != 404) {
                             array_push($new_file,$file[$j]->images);
                         }
                         
                         curl_close($handle);
+
                     }
+
                 }
                 $med_program[$i]->media_path_2 = $new_file;
-                dd( $med_program);
 
             }
             return response()->json([
